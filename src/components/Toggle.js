291d19f5f0ec383e649d53514bd466327c22d722
Arthur V Fain <arthurvfain@gmail.com>
2021-06-14 10:52:15 -0400

    commit tree cdb8534e1d8abe919275ab8650e1a550ff8ec19a

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useState } from "react"
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [toggle, setToggle] = useState(false)
+
+  function toggles()
+  {
+    setToggle((toggle) => !toggle)
+  }
+  
+  const color = toggle ? "red" : "green"
+
+  return <button onClick={toggles} style = {{background: color}}>{toggle ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
